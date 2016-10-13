1ef6ef2aaa1074c64edcba2957a01a6069cc7e61
Dohyung Ahn <emaren84@gmail.com>
2016-10-13 07:36:16 +0000

    render index view (first version)

--- app/assets/javascripts/components/records.js.jsx
+++ app/assets/javascripts/components/records.js.jsx
@@ -1,9 +1,35 @@
 var Records = React.createClass({
+  getInitialState: function() {
+    return { records: this.props.data };
+  },
+  
+  getDefaultProps: function () {
+    return { records: [] }
+  },
+  
   render: function() {
+    var rows = [];
+    
+    this.state.records.forEach(function (record) {
+      rows.push(<Record key={record.id} record={record} />)
+    });
+    
     return(
       <div className="records">
         <h2 className="title"> Records </h2>
+        <table className="ui celled striped table">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Title</th>
+              <th>Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {rows}
+          </tbody>
+        </table>
       </div>
     );
-  };
+  }
 });
